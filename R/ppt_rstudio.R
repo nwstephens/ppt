78c6ff3ced48ee6542718fbbb99bf0464face0a4
Your Name <nwstephens@gmail.com>
2018-06-01 23:21:36 -0400

    first template

--- R/ppt_rstudio.R
+++ R/ppt_rstudio.R
@@ -1,11 +1,9 @@
-ppt_rstudio <- function(toc = TRUE) {
+ppt_rstudio <- function() {
   
   potx <- system.file("rmarkdown/templates/ppt_rstudio/resources/template.potx", package = "ppt")
   
   # call the base html_document function
   rmarkdown::powerpoint_presentation(fig_width = 6.5,
                            fig_height = 4,
-                           highlight = NULL,
-                           theme = NULL,
                            reference_doc = potx)
 }
